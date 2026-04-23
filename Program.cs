5331a887ae0f701f660cae7780b28e1d0486070d
joao <joaopedroazevedo1980@gmail.com>
2026-04-22 21:44:13 -0300

    TempoDeDownload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿double mb, mbps, download;
+Console.WriteLine("--- Tempo de Download ---");
+Console.Write("Tamanho do arquivo em MB........:");
+mb = Convert.ToDouble(Console.ReadLine());
+Console.Write("Velocidade da conexão em Mbps...:");
+mbps = Convert.ToDouble(Console.ReadLine());
+download = mb * 8 / mbps / 60;
+
+Console.WriteLine($"\nTempo estimado de download: {download:N1} minutos");
